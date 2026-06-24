d6dbd8122ff895ed208004a1ec0061696d09620a
Ka1999 <karenvane1024@gmail.com>
2026-06-24 18:21:50 -0500

    Uso primera función

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ sesions = []
 tutors = []
 
 def add_student():
-    pass
+    id_student = int(input("Ingresa tu id: "))
+    name_student = input ("Ingresa tu nombre: ")
+    career_student = input ("Ingresa tu carrera: ")
+    
+    student = {
+        "id" : id_student,
+        "name" : name_student,
+        "career" : career_student
+     }
+    global students
+    students.append(student)
+    print(students)
 
 def add_subject():
     pass
@@ -14,3 +25,7 @@ def add_session():
 
 def add_tutor():
     pass
+
+add_student()
+add_student()
+add_student()
