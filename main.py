088cd8f3c81529922969da48d3d91f21204e8024
Ka1999 <karenvane1024@gmail.com>
2026-06-24 18:01:29 -0500

    Definición de funciones

---
+++ main.py
@@ -0,0 +1,16 @@
+students = []
+subjects = []
+sesions = []
+tutors = []
+
+def add_student():
+    pass
+
+def add_subject():
+    pass
+
+def add_session():
+    pass
+
+def add_tutor():
+    pass
