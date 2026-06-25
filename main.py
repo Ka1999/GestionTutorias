cd49013cec69fbbddec54ef78a374265d52c15c4
Ka1999 <karenvane1024@gmail.com>
2026-06-25 15:43:44 -0500

    Agregar funciones para sesiones y tutores

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 students = []
 subjects = []
-sesions = []
+sessions = []
 tutors = []
 
 def add_student():
     id_student = int(input("Ingresa tu id: "))
-    name_student = input ("Ingresa tu nombre: ")
-    career_student = input ("Ingresa tu carrera: ")
+    name_student = input("Ingresa tu nombre: ")
+    career_student = input("Ingresa tu carrera: ")
     
     student = {
         "id" : id_student,
@@ -18,13 +18,44 @@ def add_student():
     print(students)
 
 def add_subject():
-    pass
+    id_subject = int(input("Ingresa el id de la materia: "))
+    name_subject = input("Ingresa el nombre de la materia: ")
+
+    subject = {
+        "id" : id_subject,
+        "name" : name_subject
+        }
+    global subjects
+    subjects.append(subject)
+    print(subjects)
 
 def add_session():
-    pass
+    id_student = int(input("Ingresa tu id: "))
+    id_tutor =  input("Ingresa el nombre de la materia: ")
+    id_subject = int(input("Ingresa el id de la materia: "))
+    hour_sesion = int(input("Ingresa tu id: "))
+
+    session = {
+        "id_student" : id_student,
+        "id_tutor" : id_tutor,
+        "id_subject" : id_subject,
+        "hour_sesion" : hour_sesion
+    }
+    global sessions
+    sessions.append(session)
+    print(sessions)
 
 def add_tutor():
-    pass
+    id_tutor = input("Ingresa el nombre de la materia: ")
+    name_tutor = input("Ingresa tu nombre: ")
+
+    tutor = {
+        "id_tutor" : id_tutor,
+        "name_tutor" : name_tutor
+    }
+    global tutors
+    tutors.append(tutor)
+    print(tutors)
 
 add_student()
 add_student()
